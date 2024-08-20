a69effe3e85bfb79b7b5b971010d97fb93a56bb4
MAsadMughal <2020se10@student.uet.edu.pk>
2024-08-21 00:00:30 +0500

    final

--- DSA (CODE HELP)/Lec62_BST.cpp
+++ DSA (CODE HELP)/Lec62_BST.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 using namespace std;
 
 class node
@@ -19,7 +20,7 @@ public:
 
 node *createTree(node *root)
 {
-    cout << "Enter data" << endl;
+    cout << "Enter Data" << endl;
     int data;
     cin >> data;
     root = new node(data);
@@ -27,19 +28,19 @@ node *createTree(node *root)
     {
         return NULL;
     }
-
-    cout << "Enter data for left of " << data << endl;
+    cout << "Enter Data for left of " << data << endl;
     root->left = createTree(root->left);
-    cout << "Enter data for right of " << data << endl;
+    cout << "Enter Data for right of " << data << endl;
     root->right = createTree(root->right);
     return root;
 }
 
-void traversal(node *root)
+void levelOrderTraversal(node *root)
 {
     queue<node *> q;
     q.push(root);
     q.push(NULL);
+
     while (!q.empty())
     {
         node *temp = q.front();
@@ -59,6 +60,7 @@ void traversal(node *root)
             {
                 q.push(temp->left);
             }
+
             if (temp->right)
             {
                 q.push(temp->right);
@@ -66,8 +68,126 @@ void traversal(node *root)
         }
     }
 }
+
+void reverseOrderTraversal(node *root)
+{
+    queue<node *> q;
+    stack<node *> s;
+    q.push(root);
+    while (!q.empty())
+    {
+        root = q.front();
+        q.pop();
+        s.push(root);
+
+        if (root->right)
+        {
+            q.push(root->right);
+        }
+        if (root->left)
+        {
+            q.push(root->left);
+        }
+    }
+    while (!s.empty())
+    {
+        cout << s.top()->data << " ";
+        s.pop();
+    }
+}
+void preOrderTraversal(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+void inOrderTraversal(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrderTraversal(root->left);
+    cout << root->data << " ";
+    inOrderTraversal(root->right);
+}
+void postOrderTraversal(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+// IN ORDER TRAVERSAL using Stack
+void inorder(node *root)
+{
+    vector<int> ans;
+    stack<node *> s;
+    node *curr = root;
+    while (curr != NULL || !s.empty())
+    {
+        while (curr != NULL)
+        {
+            s.push(curr);
+            curr = curr->left;
+        }
+
+        curr = s.top();
+        s.pop();
+        ans.push_back(curr->data);
+        curr = curr->right;
+    }
+}
+
+void preorder(node *root)
+{
+    vector<int> ans;
+    stack<node *> s;
+    node *curr = root;
+    while (curr != NULL || !s.empty())
+    {
+        while (curr != NULL)
+        {
+            ans.push_back(curr->data);
+            if (curr->left == NULL)
+            {
+                s.push(curr);
+            }
+            curr = curr->left;
+        }
+
+        curr = s.top()->right;
+        s.pop();
+        ans.push_back(curr->data);
+        curr = curr->right;
+    }
+}
+
 int main()
 {
-    node *root = createTree(root);
-    traversal(root);
+    node *root = NULL;
+    root = createTree(root);
+    // 1 2 3 -1 -1 4 -1 -1 5 7 -1 -1 6 -1 -1
+    cout << "Level Order Traversal" << endl;
+    levelOrderTraversal(root);
+
+    cout << "Reverse Level Order Traversal" << endl;
+    reverseOrderTraversal(root);
+    cout << endl;
+    cout << "Pre Order Traversal" << endl;
+    preOrderTraversal(root);
+    cout << endl;
+    cout << "In-Order Traversal" << endl;
+    inorder(root);
+    cout << endl;
+    cout << "Post Order Traversal" << endl;
+    postOrderTraversal(root);
 }
